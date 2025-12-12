3dcb78d22f0a4bd90e94030f07d176cfad3917a4
Oscar Sorensen <oscar081100@gmail.com>
2025-12-12 09:47:59 +0100

    prog

--- 1-Programacion/010.-Programacion-del-servidor/009-Implantacion-CRUD/101-Ejercicios/admin/escritorio.php
+++ 1-Programacion/010.-Programacion-del-servidor/009-Implantacion-CRUD/101-Ejercicios/admin/escritorio.php
@@ -26,11 +26,14 @@ http://localhost:8080/1-Programacion/010.-Programacion-del-servidor/009-Implanta
     </nav>
     <main>
     	<?php
+      	// Esto se conoce como router (enrutador) /////////////
       	if(isset($_GET['accion'])){
         	if($_GET['accion'] == "nuevo"){
           	include "inc/create/formulario.php";
-          }else if($_GET['accion'] == "eliminar"){ // Defino la acción eliminar
-          	include "inc/delete/eliminar.php";		// En ese caso incluyo eliminar.php
+          }else if($_GET['accion'] == "eliminar"){ 					// Defino la acción eliminar
+          	include "inc/delete/eliminar.php";							// En ese caso incluyo eliminar.php
+          }else if($_GET['accion'] == "editar"){ 						// Defino la acción editar
+          	include "inc/update/formularioactualizar.php";	// En ese caso incluyo el formulario de la edicion.php
           }
         }else{
       		include "inc/read/leer.php"; 
