a67f5dfab786a9d9f28ec50a4ec9d17dbfe536d5
Oscar Sorensen <oscar081100@gmail.com>
2025-12-12 09:15:33 +0100

    first class

--- 1-Programacion/010.-Programacion-del-servidor/009-Implantacion-CRUD/101-Ejercicios/admin/escritorio.php
+++ 1-Programacion/010.-Programacion-del-servidor/009-Implantacion-CRUD/101-Ejercicios/admin/escritorio.php
@@ -15,7 +15,7 @@ http://localhost:8080/1-Programacion/010.-Programacion-del-servidor/009-Implanta
 <!doctype html>
 <html lang="es">
 	<head>
-  	<title>El Oscar - Panel de control</title>
+  	<title>El jocarsa - Panel de control</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/estilo.css">
   </head>
@@ -26,12 +26,14 @@ http://localhost:8080/1-Programacion/010.-Programacion-del-servidor/009-Implanta
     </nav>
     <main>
     	<?php
-      	if(isset($_GET['accion'])){								// Si hay "accion" en la URL
-        	if($_GET['accion'] == "nuevo"){					// Si la acción es "nuevo"
-          	include "inc/create/formulario.php";	// En ese caso mete el formulario
+      	if(isset($_GET['accion'])){
+        	if($_GET['accion'] == "nuevo"){
+          	include "inc/create/formulario.php";
+          }else if($_GET['accion'] == "eliminar"){ // Defino la acción eliminar
+          	include "inc/delete/eliminar.php";		// En ese caso incluyo eliminar.php
           }
-        }else{																		// En caso contrario
-      		include "inc/read/leer.php"; 						// Enseñame la tabla
+        }else{
+      		include "inc/read/leer.php"; 
         }
       ?>
       <a href="?accion=nuevo" id="nuevo">+</a>
--- 1-Programacion/010.-Programacion-del-servidor/009-Implantacion-CRUD/101-Ejercicios/admin/inc/read/leer.php
+++ 1-Programacion/010.-Programacion-del-servidor/009-Implantacion-CRUD/101-Ejercicios/admin/inc/read/leer.php
@@ -16,6 +16,10 @@
         echo "<td>".$fila['fecha_publicacion']."</td>";
         echo "<td>".$fila['autor_id']."</td>";
         echo "<td>".$fila['contenido']."</td>";
+        // NUEVO ///////////////
+        echo "<td><a href='?accion=editar&id=".$fila['id']."' class='editar' title='Cuidado que vas a editar un dato'>🖋</a></td>";
+        echo "<td><a href='?accion=eliminar&id=".$fila['id']."' class='eliminar' title='MAS cuidado todavía porque vas a ELIMINAR un dato'>✖</a></td>";
+      	// NUEVO ///////////////
       echo "</tr>";
     }
 
