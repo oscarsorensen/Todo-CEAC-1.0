fa79a192b6c6976a3a8cae373e77428cab1b97da
Oscar Sorensen <oscar081100@gmail.com>
2025-12-17 09:35:07 +0100

    update

---
+++ 1-Programacion/010.-Programacion-del-servidor/011-Supercontrolador/004-capricho-usualidad/back/controladores/insertar.php
@@ -0,0 +1,49 @@
+
+<form action="" method="POST">
+<?php
+      // Formulario dinamico //////////////////
+
+        $resultado = $conexion->query("
+          SELECT * FROM ".$_GET['tabla']." LIMIT 1;
+        ");	// SOLO QUIERO UN ELEMENTO !!!!!!!!!!!!!!!!
+        while ($fila = $resultado->fetch_assoc()) {
+          foreach($fila as $clave=>$valor){
+            echo "
+            	<div class='control_formulario'>
+                <label>".$clave."</label>
+                <input 
+                  type='text' 
+                  name='".$clave."'
+                  placeholder='".$clave."'>
+              </div>
+              ";
+          }
+         }
+      ?>
+</form>
+
+<style>
+	form{
+  	width:100%;
+    display:flex;
+    flex-direction:column;
+    gap:20px;
+  }
+  .control_formulario{
+  	display:flex;
+  }
+  label{
+  	flex:1;
+  }
+  input{
+  	flex:4;
+  }
+
+  input{
+  	flex:4;
+    padding:10px;
+    border:2px solid darkblue;
+  }
+
+</style>
+
