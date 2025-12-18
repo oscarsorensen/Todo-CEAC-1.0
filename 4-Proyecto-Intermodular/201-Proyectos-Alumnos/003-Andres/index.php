93c1d460d5d919f06ec71a824ecbda06bc2d9db8
Oscar Sorensen <oscar081100@gmail.com>
2025-12-18 12:39:12 +0100

    timer jocarsa

---
+++ 4-Proyecto-Intermodular/201-Proyectos-Alumnos/003-Andres/index.php
@@ -0,0 +1,15 @@
+<!doctype>
+<html>
+	<head>
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    	#ruleta{animation:frena 10s;}
+      @keyframes frena{
+      	
+      }
+    </style>
+  </head>
+  <body>
+  	<img id="ruleta" src="ruleta.jpg">
+  </body>
+</html>
