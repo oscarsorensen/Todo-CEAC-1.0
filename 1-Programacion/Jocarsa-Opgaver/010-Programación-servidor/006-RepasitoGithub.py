f7f584bfa89b34583e5f15c98d17737a6cc63a7a
Oscar Sorensen <oscar081100@gmail.com>
2026-01-02 10:17:40 +0100

    opgave

--- 1-Programacion/Jocarsa-Opgaver/010-Programación-servidor/006-RepasitoGithub.py
+++ 1-Programacion/Jocarsa-Opgaver/010-Programación-servidor/006-RepasitoGithub.py
@@ -1,22 +1,35 @@
 
 
+# ----------- 1-4 -----------
 class Gimnasio:
     def __init__(self):
-        self.nombre = nombre
-        self.apellidos = apellidos
-        self.__miembros = []   # lista privada
+        self.__miembros = [] # Lista privada para almacenar los nombres de los miembros
 
-    def anadirMiembro(self, nombre, apellidos):
-        self.__miembros.append(nombre)
+    def anadirMiembro(self, nombre):
+        self.__miembros.append(nombre) # Añadir un nuevo miembro a la lista
+
+    def contarMiembros(self):
+        return len(self.__miembros) # Devolver el número de miembros actuales
+
+    def mostrarMiembros(self):
+        for m in self.__miembros: # Mostrar todos los nombres de los miembros
+            print(m)
 
 
+# ----------- 5 (methodo main) -----------
+if __name__ == "__main__":
+    gym = Gimnasio()
 
-# ------------ 2 Herencia y polimorfismo ------------
+    gym.anadirMiembro("Oscar")
+    gym.anadirMiembro("Sofia")
+    gym.anadirMiembro("Carlos")
 
-class MiembroGimnasio(Persona):
-  def __init__(self,nombre,apellidos,edad):
-    super().__init__(nombre, apellidos)
-    self.edad = edad
+    print("Miembros actuales:", gym.contarMiembros())
+    gym.mostrarMiembros()
 
-  def dameDatos(self):
-    return "Miembro de gimnasio:"+self.nombre+" "+self.apellidos+"; Edad:"+str(self.edad) # Sobrescribo el otro dameDatos
+# Output:
+
+Miembros actuales: 3
+Oscar
+Sofia
+Carlos
