512c84044be00f01f02003e2be09a168fdccef5c
Oscar Sorensen <oscar081100@gmail.com>
2026-01-27 11:05:13 +0100

    programacion

---
+++ 1-Programacion/010.-Programacion-del-servidor/013-Idiomas-programacion/003-array.php
@@ -0,0 +1,29 @@
+<?php 
+
+    $idioma['es']['inicio'] = "Inicio";
+    $idioma['es']['sobremi'] = "Sobre mi";
+    $idioma['es']['proyectos'] = "Proyectos";
+    $idioma['es']['Contacto'] = "Contacto";
+
+?>
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Multi idioma</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <select>
+      <option value="es">🇪🇸</option>
+      <option value="en">🇬🇧</option>
+    </select>
+    <h1>Oscar Sorensen</h1>
+    <nav>
+      <a href="">Inicio</a>
+      <a href="">Sobre mi</a>
+      <a href="">Proyectos</a>
+      <a href="">Contacto</a>
+    </nav>
+  </body>
+</html>
